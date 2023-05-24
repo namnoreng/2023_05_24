5443aca4b1677170f06971ae256ec88f48efa2c5
남효식 <namnoreng@gmail.com>
2023-05-24 11:59:11 +0900

    노가다 싫어

--- main.c
+++ main.c
@@ -6,19 +6,45 @@
 #include <time.h>
 #include <math.h>
 #include <ctype.h>
+#define PI 3.141592
+
+//void main() {
+//	printf("%.1f %.1f %.1f \n",pow(2,2),pow(2,3),pow(2,4));
+//	printf("abs(-10) = %d \n",abs(-10));
+//	printf("sqrt(81) = %.1f \n",sqrt(81.0));
+//	printf("log(e) = %.1f \n",log(2,74));
+//	printf("log10(100) = %.1f \n",log10(100.0));
+//	printf("sin(30deg.) = %.1f \n",sin(PI/6));
+//	printf("cos(60deg.) = %.1f \n",cos(PI/3));
+//}
+
+//void main() {
+//	char ch;
+//	while (ch = _getch() != 'c') {
+//		printf("#");
+//	}
+//	printf("\n");
+//	while (!_kbhit()) {
+//		putchar('?');
+//		/*for (int i = 1;i < 40000;i++) {
+//			for (int j = 1;j < 10000;j++);
+//		}*/
+//		Sleep(500);
+//	/*	putchar('!');
+//		Sleep(500);*/
+//	}
+//}
 
 //int main(void) {
 //	char c;
 //	printf("%c %c \n", toupper('a'), tolower('B'));
 //	printf("%d %d \n", isupper('A'), islower('A'));
 //
-//	for (c = 'A';c <= 'I';c++) {
+//	for (c = 'a';c <= 'z';c++) {
 //		printf("%cÀÇ ASCII ÄÚµå = %x \n", c, c);
 //	}
 //	return 0;
 //}
-
-
 
 //void main() {
 //	int num;
